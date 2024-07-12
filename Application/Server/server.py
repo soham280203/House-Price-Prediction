3c42532a7aa3c9193abbe5ec31394c2b17f91cdb
Soham Khadke <sohamshekharkhadke@gmail.com>
2024-07-13 02:30:14 +0530

    GET / POST work

--- Application/Server/server.py
+++ Application/Server/server.py
@@ -1,89 +1,33 @@
-from flask  import Flask,request,jsonify
+from flask import Flask, request, jsonify
 import util
+
 app = Flask(__name__)
 
-@app.route('/hello') # Testing fUNCTION
+@app.route('/hello')  # Testing function
 def hello():
     return "Hi"
 
 @app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
-        'locations':util.get_location_names()
-
+        'locations': util.get_location_names()
     })
-    response.headers.add('Access Control-Allow-Origin','*')
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-@app.route('./predict_home_price',methods= ['POST'])
+@app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
-    total_sqft= float(request.form['total_sqft'])
+    total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
     bhk = int(request.form['bhk'])
     bath = int(request.form['bath'])
     response = jsonify({
-        'estimated_price':util.get_estimated_price(location,total_sqft,bhk,bath)
-
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
     })
-    
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
-if __name__== "__main__":
-    print("ukjk")
+if __name__ == "__main__":
+    print("Starting Flask server for Home Price Prediction...")
+    util.load_saved_artifacts()  # Load artifacts when starting the server
     app.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Application/Server/util.py
+++ Application/Server/util.py
@@ -1,44 +1,51 @@
 import json
-import pickle,numpy as np
+import pickle
+import numpy as np
 import warnings
+
 warnings.filterwarnings("ignore", message="X does not have valid feature names, but LinearRegression was fitted with feature names")
 
-__locations =None
-__data_columns =None
-__model =None
+__locations = None
+__data_columns = None
+__model = None
+__artifacts_loaded = False  # Flag to track if artifacts are loaded
 
-def get_estimated_price(location, sqft,bhk,bath):
+def get_estimated_price(location, sqft, bhk, bath):
+    load_saved_artifacts()
     try:
         loc_index = __data_columns.index(location.lower())
     except:
-        loc_index=-1    
-    x = np.zeros(len( __data_columns))
+        loc_index = -1    
+    x = np.zeros(len(__data_columns))
     x[0] = sqft
     x[1] = bhk 
     x[2] = bath
-    if loc_index>=0:
-        x[loc_index]=1
-    return round(__model.predict([x])[0] ,2)
+    if loc_index >= 0:
+        x[loc_index] = 1
+    return round(__model.predict([x])[0], 2)
+
 def get_location_names():
+    load_saved_artifacts()
     return __locations
 
 def load_saved_artifacts():
-    print("loading saved artifacts")
     global __data_columns
     global __locations
     global __model
-    with open("./Application/Artifacts/Bengaluru_House_Data.pickle",'rb') as f:
-        __model = pickle.load(f)
-    print('Loading Artifacts done')
+    global __artifacts_loaded
 
-    with open("./Application/Artifacts/columns.json",'r') as f:
-        __data_columns=json.load(f)['data_columns']
-        __locations = __data_columns[3:]
-        
+    if not __artifacts_loaded:
+        print("Loading saved artifacts")
+        with open("./Application/Artifacts/Bengaluru_House_Data.pickle", 'rb') as f:
+            __model = pickle.load(f)
+        with open("./Application/Artifacts/columns.json", 'r') as f:
+            __data_columns = json.load(f)['data_columns']
+            __locations = __data_columns[3:]
+        __artifacts_loaded = True
+        print('Loading Artifacts done')
 
-if __name__== "__main__":
+if __name__ == "__main__":
     load_saved_artifacts()
-    #print(get_location_names())
-    print(get_estimated_price("1st Phase JP Nagar",1000,3,3))
-    print(get_estimated_price("1st Phase JP Nagar",1000,2,2))
-    print(get_estimated_price("indira Nagar",1500,3,2))
+    print(get_estimated_price("1st Phase JP Nagar", 1000, 3, 3))
+    print(get_estimated_price("1st Phase JP Nagar", 1000, 2, 2))
+    print(get_estimated_price("Indira Nagar", 1500, 3, 2))
