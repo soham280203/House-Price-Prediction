7562b69c41dcd8b8592ddec74b969bd2780ba063
Soham Khadke <sohamshekharkhadke@gmail.com>
2024-07-13 19:16:25 +0530

    Final Application Completed

--- Application/Server/server.py
+++ Application/Server/server.py
@@ -31,4 +31,3 @@ if __name__ == "__main__":
     print("Starting Flask server for Home Price Prediction...")
     util.load_saved_artifacts()  # Load artifacts when starting the server
     app.run()
-n
